031891b1fdc8f2da1c265d394164c8fc76286fe2
Nuzhat-Nasrum <nuzhatnasrum111.gmail.com>
2024-09-12 17:00:11 +0600

    variable declared

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.calc;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView result;
+    double num1,num2,ans;
+    char c='0';
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
